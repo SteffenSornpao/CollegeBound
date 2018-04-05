c152ad9c6cfe339d5a4df1a09d70d7d2ef44109e
tesh-bhatia <nbhatia03@gmail.com>
2018-04-04 22:36:39 -0400

    Gives error message (but still works) on browsers w/o WebSpeech support

--- public/js/app.js
+++ public/js/app.js
@@ -3,58 +3,56 @@ if (performance.navigation.type == 1) {
   sessionStorage.clear();
 }
 try {
+  //see if browser can use speech recognition
     var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
     var recognition = new SpeechRecognition();
+
+    recognition.onstart = function() {
+      console.log('Voice recognition activated.');
+      // active()
+    }
+    
+    function active(){
+      $("#speak").animate({backgroundColor: "#ff0"}, 500)
+      $("#speak").animate({backgroundColor: "#fff"}, 500)
+      active()
+    }
+    
+    recognition.onspeechend = function() {
+      console.log('You were quiet for a while so voice recognition turned itself off.');
+    //setTimeout necessary to get query (for now)
+      setTimeout(function(){
+        submitQuery()
+        $("input").val("")
+      }, 500)
+    }
+    
+    recognition.onerror = function(event) {
+      if(event.error == 'no-speech') {
+        console.log('No speech was detected. Try again.');
+      };
+    }
+    
+      recognition.onresult = function(event) {
+        var noteContent =''
+        // event is a SpeechRecognitionEvent object.
+        // It holds all the lines we have captured so far.
+        // We only need the current one.
+        var current = event.resultIndex;
+    
+        // Get a transcript of what was said.
+        var transcript = event.results[current][0].transcript;
+    
+        // Add the current transcript to the contents of our Note.
+        noteContent += transcript;
+        $('#input').val(noteContent);
+        console.log(event.results)
+      }
   }
 catch(e) {
-  alert('Nope!')
   console.error(e);
-  $('.no-browser-support').show();
-  $('.app').hide();
+  $('#start-info').append('<p>Please note: Your current browser does not support voice searches</p>')
 }
-
-recognition.onstart = function() {
-  console.log('Voice recognition activated.');
-  // active()
-}
-
-function active(){
-  $("#speak").animate({backgroundColor: "#ff0"}, 500)
-  $("#speak").animate({backgroundColor: "#fff"}, 500)
-  active()
-}
-
-recognition.onspeechend = function() {
-  console.log('You were quiet for a while so voice recognition turned itself off.');
-//setTimeout necessary to get query (for now)
-  setTimeout(function(){
-    submitQuery()
-    $("input").val("")
-  }, 500)
-}
-
-recognition.onerror = function(event) {
-  if(event.error == 'no-speech') {
-    console.log('No speech was detected. Try again.');
-  };
-}
-
-  recognition.onresult = function(event) {
-    var noteContent =''
-    // event is a SpeechRecognitionEvent object.
-    // It holds all the lines we have captured so far.
-    // We only need the current one.
-    var current = event.resultIndex;
-
-    // Get a transcript of what was said.
-    var transcript = event.results[current][0].transcript;
-
-    // Add the current transcript to the contents of our Note.
-    noteContent += transcript;
-    $('#input').val(noteContent);
-    console.log(event.results)
-  }
-
 
 var paramS = [],
     paramSkey = [],
@@ -298,7 +296,11 @@ $("#speak").on("mouseleave", function(){
   $("#speak").animate({backgroundColor: "#fff"}, 500)
 })
 $('#speak').on('click', function(e) {
-  recognition.start();
+  if(recognition){
+    recognition.start();
+  }else{
+    alert('Voice search has been disabled on this browser!')
+  }
 });
 
 // *** Click param ***
